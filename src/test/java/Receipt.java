da4a8b585a97af997071ba4c43edc1f42ac53248
MacBook <chenyan900520@126.com>
2020-02-12 14:13:51 +0800

    ac

--- src/test/java/Receipt.java
+++ src/test/java/Receipt.java
@@ -31,8 +31,12 @@ public class Receipt {
     private static final String OUTPUT_FILE = "receipt.pdf";
     private static final String UTF_8 = "UTF-8";
 
-    @Test
-    public void generatePdf() throws Exception {
+    public static void main(String[] args) throws Exception {
+        generatePdf();
+    }
+
+
+    public static void generatePdf() throws Exception {
 
         // We set-up a Thymeleaf rendering engine. All Thymeleaf templates
         // are HTML-based files located under "src/test/resources". Beside
@@ -97,7 +101,7 @@ public class Receipt {
         outputStream.close();
     }
 
-    private List<User> exampleDataForJohnDoe() {
+    private static List<User> exampleDataForJohnDoe() {
         List<User> userList = new ArrayList<User>();
 
         User tom = new User("Tom", 19, 1);
@@ -111,55 +115,8 @@ public class Receipt {
         return userList;
     }
 
-    static class Data {
-        private String firstname;
-        private String lastname;
-        private String street;
-        private String zipCode;
-        private String city;
 
-        public String getFirstname() {
-            return firstname;
-        }
-
-        public void setFirstname(String firstname) {
-            this.firstname = firstname;
-        }
-
-        public String getLastname() {
-            return lastname;
-        }
-
-        public void setLastname(String lastname) {
-            this.lastname = lastname;
-        }
-
-        public String getStreet() {
-            return street;
-        }
-
-        public void setStreet(String street) {
-            this.street = street;
-        }
-
-        public String getZipCode() {
-            return zipCode;
-        }
-
-        public void setZipCode(String zipCode) {
-            this.zipCode = zipCode;
-        }
-
-        public String getCity() {
-            return city;
-        }
-
-        public void setCity(String city) {
-            this.city = city;
-        }
-    }
-
-    private String convertToXhtml(String html) throws UnsupportedEncodingException {
+    private static String convertToXhtml(String html) throws UnsupportedEncodingException {
         Tidy tidy = new Tidy();
         tidy.setInputEncoding(UTF_8);
         tidy.setOutputEncoding(UTF_8);
